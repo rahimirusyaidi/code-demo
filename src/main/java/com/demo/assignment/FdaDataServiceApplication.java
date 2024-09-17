a626bb3c0fb60d4a88b056a96705765614d9ea2b
RahimiRusyaidi.Riduan <rahimi.riduan@accenture.com>
2024-09-17 21:45:16 +0800

    Initial commit

---
+++ src/main/java/com/demo/assignment/FdaDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdaDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdaDataServiceApplication.class, args);
+	}
+
+}
