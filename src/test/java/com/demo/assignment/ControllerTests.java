86ea630548ce55bcea04bc80c924ea6f9a4e7bbb
rahimirusyaidi <rahimi.rusyaidi@gmail.com>
2024-09-19 15:04:07 +0800

    chore: add unit test to GET method fetch local data endpoint

--- src/test/java/com/demo/assignment/ControllerTests.java
+++ src/test/java/com/demo/assignment/ControllerTests.java
@@ -1,5 +1,6 @@
 package com.demo.assignment;
 
+import com.demo.assignment.dao.CustomDrug;
 import com.demo.assignment.model.Drug;
 import com.demo.assignment.model.DrugCatalogueBaseResponse;
 import com.demo.assignment.model.ResultInfo;
@@ -11,18 +12,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -98,4 +108,39 @@ public class ControllerTests {
 
         assertEquals(result.getResponse().getStatus(), HttpStatus.CREATED.value());
     }
-}
+
+    @DisplayName(value = "GET Method Success Call - /api/drug")
+    @Test
+    public void successScenarioCallToFetchLocalData(@Autowired MockMvc mockMvc) throws Exception {
+        CustomDrug mockDrug = new CustomDrug();
+        mockDrug.setApplicationId(UUID.randomUUID());
+        mockDrug.setManufacturerName("Homeopathy");
+        mockDrug.setSubstanceName(Arrays.asList("Camphor", "Ethyl"));
+
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<CustomDrug> mockPage = new PageImpl<>(Collections.singletonList(mockDrug), pageable, 1);
+
+
+        when(drugCatalogueService.fetchLocalDrugCatalogue(isA(String.class), any(Pageable.class)))
+                .thenReturn(mockPage);
+
+        mockMvc.perform(get("/api/drug")
+                        .param("manufacturerName", "Homeopathy")
+                        .param("page", "0")
+                        .param("size", "2")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].applicationId").exists())
+                .andExpect(jsonPath("$.content[0].manufacturerName").value("Homeopathy"))
+                .andExpect(jsonPath("$.content[0].substanceName[0]").value("Camphor"))
+                .andExpect(jsonPath("$.content[0].substanceName[1]").value("Ethyl"))
+                .andExpect(jsonPath("$.pageable.pageNumber").value(0))
+                .andExpect(jsonPath("$.pageable.pageSize").value(2))
+                .andExpect(jsonPath("$.totalPages").value(1))
+                .andExpect(jsonPath("$.totalElements").value(1))
+                .andExpect(jsonPath("$.numberOfElements").value(1))
+                .andExpect(jsonPath("$.first").value(true))
+                .andExpect(jsonPath("$.last").value(true));
+    }
+
+    }
